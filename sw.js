8386d2001ee1394efaf3ea68bded0ec83cfa692d
yamamoke-app <yamamoteaux@gmail.com>
2026-05-13 19:28:33 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,32 +1,20 @@
-const CACHE_NAME = "counter-app-v2";
+const CACHE_NAME = "counter-app-v1";
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./manifest.json"
+];
 
+// インストール
 self.addEventListener("install", event => {
-  self.skipWaiting(); // ← これが重要（即更新）
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll([
-        "./",
-        "./index.html",
-        "./manifest.json"
-      ]);
+      return cache.addAll(urlsToCache);
     })
   );
 });
 
-self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(keys => {
-      return Promise.all(
-        keys.map(key => {
-          if (key !== CACHE_NAME) {
-            return caches.delete(key);
-          }
-        })
-      );
-    })
-  );
-});
-
+// 取得
 self.addEventListener("fetch", event => {
   event.respondWith(
     caches.match(event.request).then(response => {
